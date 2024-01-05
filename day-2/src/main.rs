49dc15893236532ad67e4961f153356d84e3f2eb
patriciogutierrez <tatito.guti@gmail.com>
2024-01-05 20:26:40 -0300

    ✨ day 2 part 1 finished

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,15 +1,26 @@
-use std::{error::Error, fs};
+use std::fs;
 
 pub struct Cubes {
     pub red: Option<i64>,
     pub green: Option<i64>,
     pub blue: Option<i64>,
 }
+
+impl Cubes {
+    fn get_property(&self, name: &str) -> Option<i64> {
+        match name {
+            "red" => self.red,
+            "green" => self.green,
+            "blue" => self.blue,
+            _ => panic!("property not defined"),
+        }
+    }
+}
 fn main() {
     let cubes = Cubes {
-        red: Some(14),
-        green: Some(2),
-        blue: Some(2),
+        red: Some(12),
+        green: Some(13),
+        blue: Some(14),
     };
     static FILE_PATH: &str = "src/testfile.txt";
     let total = fs::read_to_string(FILE_PATH)
@@ -33,25 +44,33 @@ fn count_line(line: &str, cubes: &Cubes) -> i64 {
         .unwrap()
         .to_owned();
 
-    let games = data.last().unwrap().split(";").collect::<Vec<&str>>();
-
-    let parsed_games = games
-        .into_iter()
-        .filter_map(|game| bigger_than_cubes(game, cubes).ok())
-        .collect::<Vec<()>>();
-    println!("{:?}", parsed_games);
-    if parsed_games.len() == 0 {
+    let games = data
+        .last()
+        .unwrap()
+        .split(";")
+        .collect::<Vec<&str>>()
+        .join(";");
+    if bigger_than_cubes(games.as_str(), cubes).is_err() {
         return 0;
     }
+    println!("buen index: {:?}", index);
     index
 }
 
 fn bigger_than_cubes(game: &str, cubes: &Cubes) -> Result<(), ()> {
-    println!("{}", cubes.green.unwrap());
-    if cubes.green.unwrap() > 1 {
-        println!("error");
-        return Err(());
+    let rounds = game.split(";").collect::<Vec<&str>>();
+    let mut res = Ok(());
+    'round_loop: for round in rounds {
+        let colors = round.split(",").collect::<Vec<&str>>();
+        for color in colors {
+            let parsed = color.trim().split(" ").collect::<Vec<&str>>();
+            let color_value = Some(parsed.first().unwrap().parse::<i64>().unwrap());
+            let final_color = parsed.last().unwrap();
+            if color_value.gt(&cubes.get_property(final_color)) {
+                res = Err(());
+                break 'round_loop;
+            }
+        }
     }
-    println!("no hay error");
-    Ok(())
+    return res;
 }
